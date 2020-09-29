5bc2ce6ac9c389608664a81ea6500e4c2d5b9dfd
Cristina Ruiz <cristinaruiz96@yahoo.com>
2020-09-28 22:23:17 -0500

    Start page created with start button. Question and answer buttons also created

---
+++ script.js
@@ -0,0 +1,18 @@
+var startButton = document.getElementById("start-button");
+var questionEl = document.getElementById("questions");
+startButton.addEventListener("click", startQuiz);
+
+
+function startQuiz(){
+    startButton.classList.add("hide");
+    questionEl.classList.remove("hide");
+}
+
+
+function nextQuestion(){
+    
+}
+
+function selectAnswer(){
+
+}
